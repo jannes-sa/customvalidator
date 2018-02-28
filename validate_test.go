48700fe9f4fedb0a5a4167a1b8f4c2792b2897e1
jannes-sa <jannes.sa@tnis.com>
2018-02-28 12:39:28 +0700

    A

--- validate_test.go
+++ validate_test.go
@@ -49,17 +49,19 @@ func TestValidate(t *testing.T) {
 
 func TestValidateArray(t *testing.T) {
 	type ArrTesInterface struct {
-		AccountNumber interface{} `json:"account_number" type:"string" validate:"required=E02030009,stringnumericonly=E02030008,lte=20=E02030007,type=E02030001"`
-		Status        interface{} `json:"status" type:"int" validate:"required=E02030009,type=E02030001"`
-		Amount        interface{} `json:"amount" type:"float64" validate:"required=E02030009,type=E02030001"`
+		ID            interface{} `json:"id" type:"string" validate:"required=E02030009"`
+		AccountNumber interface{} `json:"account_number" type:"string" validate:"required=E02030009,stringnumericonly=E02030008,lte=20=E02030007"`
+		Status        interface{} `json:"status" type:"int" validate:"required=E02030009"`
+		Amount        interface{} `json:"amount" type:"float64" validate:"required=E02030009"`
 	}
 
 	type TesInterface2 struct {
-		TransactionAmount interface{}       `json:"transaction_amount" type:"float64" validate:"gte=0=E0209898984,lte=999999999999999=E02030010,type=E02030011,required=E02030012"`
+		TransactionAmount interface{}       `json:"transaction_amount" type:"float64" validate:"gte=0=E0209898984,lte=999999999999999=E02030010,required=E02030012"`
 		Data              []ArrTesInterface `json:"data"`
 	}
 
 	type ArrTes struct {
+		ID            string  `json:"id"`
 		AccountNumber string  `json:"account_number"`
 		Status        int     `json:"status"`
 		Amount        float64 `json:"amount"`
@@ -75,16 +77,19 @@ func TestValidateArray(t *testing.T) {
 			"transaction_amount":100000,
 			"data":[
 				{
+					"id":"a",
 					"account_number":"123456789123",
 					"status":1,
 					"amount":2000
 				},
 				{
+					"id":"b",
 					"account_number":"985654637462",
 					"status":2,
 					"amount":3000
 				},
 				{
+					"id":"c",
 					"account_number":"101010637462",
 					"status":3,
 					"amount":1500	
@@ -102,6 +107,12 @@ func TestValidateArray(t *testing.T) {
 
 	var tes Tes2
 	errCode := Validate(tesInter, &tes)
+
+	if len(errCode) > 0 {
+		t.Error("validate Fail ", errCode)
+		log.Println(errCode, tes)
+		return
+	}
 
 	log.Println(errCode, tes)
 
